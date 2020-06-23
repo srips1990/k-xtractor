ffd3d96fb2a55337062b72bb696c7549c08612b1
Cypher <srips1990@gmail.com>
2020-06-23 20:20:25 +0530

    Fixed cosmetic issues

--- static/js/index.js
+++ static/js/index.js
@@ -1,9 +1,15 @@
-function get_output(job_desc, num_words, output_element, invoker) {
+
+function get_output(job_desc, num_words, invoker) {
 	var xmlhttp = new XMLHttpRequest();
 //    var csrftoken = getCookie('csrftoken');
     var csrftoken = document.getElementsByName('csrfmiddlewaretoken')[0].value;
     invoker.disabled = true;
-    output_element.innerHTML = "<b><i>Processing...</i></b>";
+    var most_freq_container = document.getElementById('most_freq_container');
+    var least_freq_container = document.getElementById('least_freq_container');
+    var status_elem = document.getElementById('status');
+    most_freq_container.innerHTML = "";
+    least_freq_container.innerHTML = "";
+    status_elem.innerHTML = "<b><i>Processing...</i></b>";
 
 	try {
 
@@ -11,46 +17,28 @@ function get_output(job_desc, num_words, output_element, invoker) {
 
 			if (xmlhttp.readyState == 4) {
 			    invoker.disabled = false;
-                output_element.innerHTML = "";
+                status_elem.innerHTML = "";
 
 				if(xmlhttp.status == 200) {
 				    var resp_json = JSON.parse(xmlhttp.responseText);
 		            var img_most_freq = document.createElement('img');
 		            img_most_freq.setAttribute('id', 'img_most_freq')
 		            img_most_freq.setAttribute('src', resp_json.file1_name)
-//		            img_most_freq.setAttribute('width', 400)
-//		            img_most_freq.setAttribute('height', 300)
 
 		            var img_least_freq = document.createElement('img');
 		            img_least_freq.setAttribute('id', 'img_least_freq')
 		            img_least_freq.setAttribute('src', resp_json.file2_name)
-//		            img_least_freq.setAttribute('width', 400)
-//		            img_least_freq.setAttribute('height', 300)
 
-		            var image1_container = document.createElement('div');
-		            image1_container.setAttribute('class', 'contact1-pic js-tilt')
-		            image1_container.setAttribute('data-tilt', '')
-
-		            var image2_container = document.createElement('div');
-		            image2_container.setAttribute('class', 'contact1-pic js-tilt')
-		            image2_container.setAttribute('data-tilt', '')
-
-		            output_element.appendChild(document.createElement('br'))
-		            output_element.appendChild(document.createElement('br'))
-		            output_element.appendChild(document.createElement('b').appendChild(document.createTextNode('Most frequent words')))
-		            output_element.appendChild(document.createElement('br'))
-		            image1_container.appendChild(img_most_freq)
-		            output_element.appendChild(image1_container)
-		            output_element.appendChild(document.createElement('br'))
-		            output_element.appendChild(document.createElement('br'))
-		            output_element.appendChild(document.createElement('b').appendChild(document.createTextNode('Least frequent words')))
-		            output_element.appendChild(document.createElement('br'))
-		            image2_container.appendChild(img_least_freq)
-		            output_element.appendChild(image2_container)
+		            most_freq_container.appendChild(document.createElement('b').appendChild(document.createTextNode('Most frequent words')))
+		            most_freq_container.appendChild(document.createElement('br'));
+		            most_freq_container.appendChild(img_most_freq);
+		            least_freq_container.appendChild(document.createElement('b').appendChild(document.createTextNode('Least frequent words')))
+		            least_freq_container.appendChild(document.createElement('br'));
+		            least_freq_container.appendChild(img_least_freq);
 				}
 				else if (xmlhttp.status == 500) {
 					console.log(xmlhttp);
-					output_element.innerHTML = xmlhttp.statusText;
+					status_elem.innerHTML = xmlhttp.statusText;
 				}
 			}
 		};
